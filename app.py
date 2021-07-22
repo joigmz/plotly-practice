ca30bf92224d942ef888731ef446a768971aa786
jose-izam99 <57118197+jose-izam99@users.noreply.github.com>
2021-07-22 01:20:32 -0400

    fix connection

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from sklearn.neighbors import KNeighborsClassifier
 
 app = dash.Dash(__name__)
 
+server = app.server
+
 app.layout = html.Div([
     dcc.Graph(id="graph"),
     html.P("Number of neighbors:"),
